9f52dc11c5c23aad8077cd7ee8a861513acb465b
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-04-01 18:05:19 +0200

    update layout and style for 'testimonials' and 'about us' and fix footer width

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,4 @@
+import { Quote } from 'lucide-react';
 import { Button } from '../ui/button';
 import { ScrollArea, ScrollBar } from '../ui/scroll-area';
 import {
@@ -80,7 +81,9 @@ function App() {
             <div className="box-left flex flex-col gap-y-4 justify-between text-center md:text-left">
               <div>
                 <h1 className="text-5xl uppercase">
-                  Gérez votre coloc au même endroit
+                  Gérez votre coloc
+                  <br />
+                  au même endroit
                 </h1>
               </div>
               <p>
@@ -108,77 +111,167 @@ function App() {
         {/* Fake testimonials */}
 
         <section className="w-100">
-          <h2>Ils ne parlent (pas) de nous !</h2>
+          <h2 className="section-title text-center text-4xl mt-12 mb-6 text-eden-800">
+            Ils (ne) parlent (pas) de nous !
+          </h2>
+          <p className="section-subtitle text-lg text-center mb-12">
+            ... et ne le feront jamais ;)
+          </p>
           <ScrollArea id="about-menu" className="w-100 whitespace-nowrap">
             <section className="flex w-max space-x-4 p-4 overflow-hidden rounded-md">
-              <Card className="w-[300px] h-[200px]">
-                <CardHeader className="flex">
-                  <Avatar>
-                    <AvatarImage src="" />
+              <Card className="w-[300px] h-[300px]">
+                <CardHeader className="flex-row">
+                  <Avatar className="mr-3">
+                    <AvatarImage src="https://picsum.photos/200" alt="" />
                     <AvatarFallback>FR</AvatarFallback>
                   </Avatar>
-                  <CardTitle>Florian</CardTitle>
+                  <div className="flex-column">
+                    <CardTitle>Florian</CardTitle>
+                    <div className="autor-data font-light text-sm">
+                      29 ans, Paris
+                    </div>
+                  </div>
                 </CardHeader>
                 <CardContent>
-                  <p>
-                    Cette appli ma changé la vie. Je vous la recommande les yeux
-                    fermés.
+                  <Quote
+                    className="scale-x-[-1] mb-3"
+                    size={35}
+                    color="#026d65"
+                  />
+                  <p className="w-full text-left">
+                    Cette appli m&rsquo;a changé la vie. Je vous la recommande
+                    les yeux fermés.
                   </p>
+                  <Quote className="mt-3" size={35} color="#026d65" />
                 </CardContent>
               </Card>
-              <div className="shrink-0">
-                <img
-                  src="https://picsum.photos/200"
-                  alt=""
-                  className="aspect-[3/4] h-fit w-fit object-cover"
-                />
-                <p className="pt-2 text-xs text-muted-foreground">Prénom</p>
-              </div>
-
-              <div className="shrink-0">
-                <img
-                  src="https://picsum.photos/200"
-                  alt=""
-                  className="aspect-[3/4] h-fit w-fit object-cover"
-                />
-                <p className="pt-2 text-xs text-muted-foreground">Prénom</p>
-              </div>
-
-              <div className="shrink-0">
-                <img
-                  src="https://picsum.photos/200"
-                  alt=""
-                  className="aspect-[3/4] h-fit w-fit object-cover"
-                />
-                <p className="pt-2 text-xs text-muted-foreground">Prénom</p>
-              </div>
-
-              <div className="shrink-0">
-                <img
-                  src="https://picsum.photos/200"
-                  alt=""
-                  className="aspect-[3/4] h-fit w-fit object-cover"
-                />
-                <p className="pt-2 text-xs text-muted-foreground">Prénom</p>
-              </div>
-
-              <div className="shrink-0">
-                <img
-                  src="https://picsum.photos/200"
-                  alt=""
-                  className="aspect-[3/4] h-fit w-fit object-cover"
-                />
-                <p className="pt-2 text-xs text-muted-foreground">Prénom</p>
-              </div>
-
-              <div className="shrink-0">
-                <img
-                  src="https://picsum.photos/200"
-                  alt=""
-                  className="aspect-[3/4] h-fit w-fit object-cover"
-                />
-                <p className="pt-2 text-xs text-muted-foreground">Prénom</p>
-              </div>
+              <Card className="w-[300px] h-[300px]">
+                <CardHeader className="flex-row">
+                  <Avatar className="mr-3">
+                    <AvatarImage src="https://picsum.photos/200" alt="" />
+                    <AvatarFallback>NC</AvatarFallback>
+                  </Avatar>
+                  <div className="flex-column">
+                    <CardTitle>Noëllie</CardTitle>
+                    <div className="autor-data font-light text-sm">
+                      29 ans, Paris
+                    </div>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <Quote
+                    className="scale-x-[-1] mb-3"
+                    size={35}
+                    color="#026d65"
+                  />
+                  <p className="w-full text-left">
+                    Finie la galère des postits partagés&nbsp;! On gère tout au
+                    même endroit, ça nous fait gagner tellement de temps&nbsp;!
+                  </p>
+                  <Quote className="mt-3" size={35} color="#026d65" />
+                </CardContent>
+              </Card>
+              <Card className="w-[300px] h-[300px]">
+                <CardHeader className="flex-row">
+                  <Avatar className="mr-3">
+                    <AvatarImage src="https://picsum.photos/200" alt="" />
+                    <AvatarFallback>JD</AvatarFallback>
+                  </Avatar>
+                  <div className="flex-column">
+                    <CardTitle>Josette</CardTitle>
+                    <div className="autor-data font-light text-sm">
+                      75 ans, Paris
+                    </div>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <Quote
+                    className="scale-x-[-1] mb-3"
+                    size={35}
+                    color="#026d65"
+                  />
+                  <p className="w-full text-left">
+                    À mon âge, c&rsquo;est difficile de se souvenir de tout.
+                    Heureusement que CoHabit est là pour m&rsquo;aider&nbsp;!
+                  </p>
+                  <Quote className="mt-3" size={35} color="#026d65" />
+                </CardContent>
+              </Card>
+              <Card className="w-[300px] h-[300px]">
+                <CardHeader className="flex-row">
+                  <Avatar className="mr-3">
+                    <AvatarImage src="https://picsum.photos/200" alt="" />
+                    <AvatarFallback>AF</AvatarFallback>
+                  </Avatar>
+                  <div className="flex-column">
+                    <CardTitle>Adrien</CardTitle>
+                    <div className="autor-data font-light text-sm">
+                      23 ans, Paris
+                    </div>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <Quote
+                    className="scale-x-[-1] mb-3"
+                    size={35}
+                    color="#026d65"
+                  />
+                  <p className="w-full text-left">
+                    C&rsquo;est simple et efficace&nbsp;!
+                  </p>
+                  <Quote className="mt-3" size={35} color="#026d65" />
+                </CardContent>
+              </Card>
+              <Card className="w-[300px] h-[300px]">
+                <CardHeader className="flex-row">
+                  <Avatar className="mr-3">
+                    <AvatarImage src="https://picsum.photos/200" alt="" />
+                    <AvatarFallback>JA</AvatarFallback>
+                  </Avatar>
+                  <div className="flex-column">
+                    <CardTitle>Julie</CardTitle>
+                    <div className="autor-data font-light text-sm">
+                      32 ans, Paris
+                    </div>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <Quote
+                    className="scale-x-[-1] mb-3"
+                    size={35}
+                    color="#026d65"
+                  />
+                  <p className="w-full text-left">
+                    Ça a créé une super ambiance notre petite coloc&rsquo;. On
+                    pouvait difficilement rêver mieux&nbsp;! Je recommande à
+                    200&nbsp;%&nbsp;!
+                  </p>
+                  <Quote className="mt-3" size={35} color="#026d65" />
+                </CardContent>
+              </Card>
+              <Card className="w-[300px] h-[300px]">
+                <CardHeader className="flex-row">
+                  <Avatar className="mr-3">
+                    <AvatarImage src="https://picsum.photos/200" alt="" />
+                    <AvatarFallback>JA</AvatarFallback>
+                  </Avatar>
+                  <div className="flex-column">
+                    <CardTitle>Patate (le chat)</CardTitle>
+                    <div className="autor-data font-light text-sm">
+                      6 ans, Paris
+                    </div>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <Quote
+                    className="scale-x-[-1] mb-3"
+                    size={35}
+                    color="#026d65"
+                  />
+                  <p className="w-full text-left">Trop cool &nbsp;!</p>
+                  <Quote className="mt-3" size={35} color="#026d65" />
+                </CardContent>
+              </Card>
             </section>
             <ScrollBar orientation="horizontal" />
           </ScrollArea>
@@ -186,7 +279,15 @@ function App() {
 
         {/* FAQ */}
         <section>
-          <h2 id="faq-menu">Foire aux questions</h2>
+          <h2
+            id="faq-menu"
+            className="section-title text-center text-4xl mt-12 mb-6 text-eden-800"
+          >
+            Foire aux questions
+          </h2>
+          <p className="section-subtitle text-lg text-center mb-12">
+            Vos questions. Nos réponses.
+          </p>
           <Accordion type="single" collapsible className="w-full">
             <AccordionItem value="item-1">
               <AccordionTrigger>Comment fonctionne CoHabit ?</AccordionTrigger>
@@ -194,7 +295,7 @@ function App() {
             </AccordionItem>
             <AccordionItem value="item-2">
               <AccordionTrigger>
-                Est-ce que l&apos;application est gratuite ?
+                Est-ce que l&rsquo;application est gratuite ?
               </AccordionTrigger>
               <AccordionContent>Yes, it is working fine.</AccordionContent>
             </AccordionItem>
@@ -204,15 +305,24 @@ function App() {
               </AccordionTrigger>
               <AccordionContent>Yes, it is working fine.</AccordionContent>
             </AccordionItem>
+            <AccordionItem value="item-4">
+              <AccordionTrigger>To Be Or Not To Be?</AccordionTrigger>
+              <AccordionContent>Yes, it is working fine.</AccordionContent>
+            </AccordionItem>
           </Accordion>
         </section>
 
         {/* About us page */}
-        <ScrollArea
+        <h2 className="section-title text-center text-4xl mt-12 mb-6 text-eden-800">
+          Qui somme-nous&nbsp;?
+        </h2>
+        <p className="section-subtitle text-lg text-center mb-12">
+          Des passionnés comme on n&rsquo;en trouve pas partout&nbsp;!
+        </p>
+        {/* <ScrollArea
           id="about-menu"
           className="w-96 whitespace-nowrap rounded-md border"
         >
-          <h2>Qui est derrière ce projet ?</h2>
           <section className="flex w-max space-x-4 p-4 overflow-hidden rounded-md">
             <div className="shrink-0">
               <img
@@ -242,19 +352,101 @@ function App() {
             </div>
           </section>
           <ScrollBar orientation="horizontal" />
-        </ScrollArea>
+        </ScrollArea> */}
+        <div className="grid grid-cols-5 grid-rows-1 gap-5">
+          <div className="team-member-card shrink-0">
+            <img
+              src="https://picsum.photos/200"
+              alt=""
+              className="profile-picture aspect-[3/4] h-fit w-fit object-cover"
+            />
+            <p className="team-firstname pt-2 text-s text-muted-foreground">
+              Aude
+            </p>
+            <p className="team-position pt-2 text-xs text-muted-foreground">
+              Développeuse back-end
+            </p>
+          </div>
+
+          <div className="team-member-card shrink-0">
+            <img
+              src="https://picsum.photos/200"
+              alt=""
+              className="profile-picture aspect-[3/4] h-fit w-fit object-cover"
+            />
+            <p className="team-firstname pt-2 text-s text-muted-foreground">
+              Caitlyne
+            </p>
+            <p className="team-position pt-2 text-xs text-muted-foreground">
+              Développeuse back-end
+            </p>
+          </div>
+          <div className="team-member-card shrink-0">
+            <img
+              src="https://picsum.photos/200"
+              alt=""
+              className="profile-picture aspect-[3/4] h-fit w-fit object-cover"
+            />
+            <p className="team-firstname pt-2 text-s text-muted-foreground">
+              Florian
+            </p>
+            <p className="team-position pt-2 text-xs text-muted-foreground">
+              Développeur front-end
+            </p>
+          </div>
+          <div className="team-member-card shrink-0">
+            <img
+              src="https://picsum.photos/200"
+              alt=""
+              className="profile-picture aspect-[3/4] h-fit w-fit object-cover"
+            />
+            <p className="team-firstname pt-2 text-s text-muted-foreground">
+              Mickaël
+            </p>
+            <p className="team-position pt-2 text-xs text-muted-foreground">
+              Développeur back-end
+            </p>
+          </div>
+          <div className="team-member-card shrink-0">
+            <img
+              src="https://picsum.photos/200"
+              alt=""
+              className="profile-picture aspect-[3/4] h-fit w-fit object-cover"
+            />
+            <p className="team-firstname pt-2 text-s text-muted-foreground">
+              Noëllie
+            </p>
+            <p className="team-position pt-2 text-xs text-muted-foreground">
+              Développeuse front-end
+            </p>
+          </div>
+        </div>
 
         {/* Contact */}
 
         <section id="contact-menu">
-          <h2>Contact</h2>
-          <p>Une question ? Une remarque ?</p>
-          <p>Contatez-nous à hello@ocoloc.com</p>
+          <h2 className="section-title text-center text-4xl mt-12 mb-6 text-eden-800">
+            Contact
+          </h2>
+          <p className="section-subtitle text-lg text-center mb-6">
+            Une question&nbsp;? Une remarque&nbsp;? On vous répond au plus
+            vite&nbsp;!
+          </p>
+          <p className="text-center font-regular">
+            Contatez-nous à{' '}
+            <a
+              className="hover:text-eden-600"
+              href="mailto:hello@ocohabit.com"
+              title="Mail to: hello@cohabit.com"
+            >
+              hello@cohabit.com
+            </a>
+          </p>
         </section>
       </main>
 
-      <footer className="py-5 bg-jet-900 text-center text-floral-white w-screen">
-        <p>CoHabit 2024 - Kimchi</p>
+      <footer className="py-5 bg-jet-900 text-center text-floral-white w-full">
+        <p>© CoHabit 2024 - Kimchi</p>
       </footer>
     </div>
   );
