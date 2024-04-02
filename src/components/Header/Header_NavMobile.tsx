70a995f3a29e4d993812cf60aff120726202dcaf
Umu <f.rumigny@gmail.com>
2024-04-02 16:31:17 +0200

    style on the close button, full width on mobile and style on the nav with separator component from shadcn

--- src/components/Header/Header_NavMobile.tsx
+++ src/components/Header/Header_NavMobile.tsx
@@ -1,15 +1,9 @@
+import { NavLink } from 'react-router-dom';
 import { Menu } from 'lucide-react';
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-  SheetClose,
-} from '../ui/sheet';
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from '../ui/sheet';
 
 import { Button } from '../ui/button';
+import { Separator } from '../ui/separator';
 
 function NavMobile() {
   return (
@@ -20,6 +14,20 @@ function NavMobile() {
             <Menu />
           </Button>
         </SheetTrigger>
+        <SheetContent className=" w-[100vw] sm:w-[400px] text-3xl">
+          <SheetHeader>Menu</SheetHeader>
+          <Separator className=" mt-5" />
+          <nav className="flex flex-col gap-5 py-6 items-center sm:items-start">
+            <a href="#faq-menu">FAQs</a>
+            <Separator />
+            <a href="#faq-menu">À propos de nous</a>
+            <Separator />
+            <a href="#faq-menu">Contact</a>
+            <Separator />
+            <NavLink to="/inscription">Créer une coloc</NavLink>
+            <Separator />
+          </nav>
+        </SheetContent>
       </Sheet>
     </div>
   );
