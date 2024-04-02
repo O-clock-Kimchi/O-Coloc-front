6ec423931476c3f0ed84bc8d442831d2a5ae5671
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-04-02 11:53:59 +0200

    set 'router' as RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router';
 
 import App from './components/App/App';
 
@@ -7,6 +9,6 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
