e1f297924a3dcede7e34483573d65e8596e80644
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-04-03 15:51:31 +0200

    create modal to allow copying secret code to invite user to join group

---
+++ src/components/Colocation/Colocation_AddFlatmateModal.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+import { X, Copy } from 'lucide-react';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+
+import { Toaster } from '../ui/toaster';
+import { useToast } from '../ui/use-toast';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
+
+function AddFlatmateModal({ onClose }) {
+  const [codeIsCopied, setCodeIsCopied] = useState(false);
+  const { toast } = useToast();
+
+  const onCopyCode = () => {
+    setCodeIsCopied(true);
+    setTimeout(() => setCodeIsCopied(false), 3000);
+  };
+
+  return (
+    <div className="modal-container flex w-4/5 h-1/4 mx-auto">
+      <Card className="bg-jet-50 w-4/5">
+        <CardHeader className="flex">
+          <Button className="flex self-end" variant="ghost">
+            <X onClick={onClose} />
+          </Button>
+
+          <CardTitle>Ajouter un.e coloc&rsquo;</CardTitle>
+          <CardDescription>
+            Transmettez-lui le code secret de la colocation
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="copy-to-clipboard-container flex space-x-3">
+            <Input
+              type="text"
+              value="00000000"
+              id="code"
+              className="flex w-3/5"
+              disabled
+            />
+            <CopyToClipboard text="00000000" onCopy={onCopyCode}>
+              <Button
+                className=""
+                variant="ghost"
+                onClick={() => {
+                  toast({
+                    description: 'Copié dans le presse-papiers !',
+                  });
+                }}
+              >
+                <Copy />
+              </Button>
+            </CopyToClipboard>
+          </div>
+          {codeIsCopied && <Toaster />}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default AddFlatmateModal;
