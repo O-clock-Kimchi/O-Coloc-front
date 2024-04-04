30731e8bd27175944af4b9079e86b0fb4510ec43
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-04-04 14:35:10 +0200

    wrap provider around RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router';
+import store from './store/index';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
