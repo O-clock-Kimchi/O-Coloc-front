d89e5201c621dd57acbe13cafab5e51bc4c3145f
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-04-04 15:16:21 +0200

    create actions 'login' and 'logout'

--- src/store/action/userAction.ts
+++ src/store/action/userAction.ts
@@ -1,1 +1,8 @@
-import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
+import { createAction } from '@reduxjs/toolkit';
+
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
+export const login = createAction(LOGIN);
+
+export const logout = createAction(LOGOUT);
