6b0a3093cbf9510d2bc828ae52c33b386c4be1a4
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-04-04 15:17:10 +0200

    add cases for 'login' and 'logout' actions

--- src/store/reducers/user.ts
+++ src/store/reducers/user.ts
@@ -2,25 +2,37 @@ import { createReducer } from '@reduxjs/toolkit';
 
 import { IUser } from '../../@types/coloc';
 
+import { login, logout } from '../action/userAction';
+
 interface UserState {
   user: IUser;
-  logged: boolean;
+  isLogged: boolean;
   loggedMessage: null | string;
 }
 
 export const initialState: UserState = {
   user: {
-    id: 1,
+    id: 3,
     firstname: 'bob',
     email: 'bob@ocoloc.com',
     password: 'bobi',
     color: '#FF0000',
     id_coloc: 2,
   },
-  logged: false,
+  isLogged: false,
   loggedMessage: null,
 };
 
-const userReducer = createReducer(initialState, (builder) => {});
+const userReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(login, (state) => {
+      state.isLogged = true;
+      state.loggedMessage = `${state.user.firstname}`;
+    })
+    .addCase(logout, (state) => {
+      state.isLogged = false;
+      state.loggedMessage = null;
+    });
+});
 
 export default userReducer;
