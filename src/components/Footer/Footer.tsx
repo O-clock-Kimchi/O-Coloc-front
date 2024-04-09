5ff9c15e3f384e72a5cc0b44812d2189448cca0d
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-04-09 10:36:41 +0200

    fix footer position by adding setting margin top to 'auto'

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="py-5 bg-jet-900 text-center text-floral-white w-full">
+    <footer className="flex flex-col py-5 bg-jet-900 text-center text-floral-white w-full mt-auto">
       <p>© CoHabit 2024 - Kimchi</p>
     </footer>
   );
