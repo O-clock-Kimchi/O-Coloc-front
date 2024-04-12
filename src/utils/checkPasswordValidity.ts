92e57ea194489363dd140816a1fd9c7b1b6c6de8
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-04-12 17:06:23 +0200

    update regex checking validity password to allow more than 1 special character anywhere in string

--- src/utils/checkPasswordValidity.ts
+++ src/utils/checkPasswordValidity.ts
@@ -1,6 +1,7 @@
 const passwordIsValid = (string: string) => {
   const passwordRegex =
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[0-9a-zA-Z!@#$%^&*]{8,}$/;
+    // /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[0-9a-zA-Z!@#$%^&*]{8,}$/;
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
   if (string.match(passwordRegex)) {
     return true;
   }
