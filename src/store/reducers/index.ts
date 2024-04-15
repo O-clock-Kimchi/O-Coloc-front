9a2aa3cf8e7522092023e71b362b37560198269c
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-04-15 16:00:51 +0200

    combine reducers into one single reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,13 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
 import userReducer from './user';
 import colocReducer from './coloc';
+import tasksReducer from './tasks';
 
-const reducer = {
+const reducer = combineReducers({
   userReducer,
   colocReducer,
-};
+  tasksReducer,
+});
 
 export default reducer;
