f50829fb7e786040c66e0bcc1558b61f5d78b72d
Umu <f.rumigny@gmail.com>
2024-04-15 14:20:00 +0200

    regex correction

--- src/utils/checkPasswordValidity.ts
+++ src/utils/checkPasswordValidity.ts
@@ -1,5 +1,5 @@
 const passwordIsValid = (string: string) => {
-  const passwordRegex = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[!@#$%^&*]).{8,}$/;
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
   if (string.match(passwordRegex)) {
     return true;
   }
