5a30a88bfc44af3b69c497d7c1c4d3c91cd19686
Umu <f.rumigny@gmail.com>
2024-05-06 14:23:34 +0200

    test deploy

--- src/store/axiosconfig.ts
+++ src/store/axiosconfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_API = import.meta.env.VITE_BASE_API;
+const BASE_API = 'https://o-coloc-back.onrender.com/';
 
 const axiosInstance = axios.create({
   baseURL: BASE_API,
